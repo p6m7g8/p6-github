3c1838387a082473a8a67e3a827e3b5da307353c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-06-16 17:14:55 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -4,12 +4,16 @@ p6df::modules::p6github::external::brew() { }
 
 p6df::modules::p6github::init() {
 
-    local dir=$P6_DFZ_DATA_DIR/p6m7g8/p6github
+  p6_github_init $P6_DFZ_SRC_DIR/p6m7g8/p6github
+}
 
-    local file
-    for file in $dir/lib/*.sh; do
-         p6_file_load "$file"
-    done
+p6_github_init() {
+  local dir="$1"
+
+  local file
+  for file in $dir/lib/*.sh; do
+    p6_file_load "$file"
+  done
 }
 
 p6df::modules::p6github::init
