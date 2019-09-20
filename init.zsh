cd3b98466e0f30cf0cd0603312264eda3aa0173d
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-09-20 17:52:49 -0400

    bare code ; bootstrap

--- init.zsh
+++ init.zsh
@@ -4,16 +4,6 @@ p6df::modules::p6github::external::brew() { }
 
 p6df::modules::p6github::init() {
 
-  p6_github_init $P6_DFZ_SRC_DIR/p6m7g8/p6github
+  local dir="$P6_DFZ_SRC_DIR/p6m7g8/p6github"
+  p6_bootstrap "$dir"
 }
-
-p6_github_init() {
-  local dir="$1"
-
-  local file
-  for file in $dir/lib/*.sh; do
-    p6_file_load "$file"
-  done
-}
-
-p6df::modules::p6github::init
