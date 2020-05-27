eea14d331c4e2e07a4888ca1ae0cfc758fec30d3
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-05-27 14:29:44 -0400

    feature(api): adds p6_github_cli_submit() (#3)

---
+++ lib/cli.sh
@@ -0,0 +1,18 @@
+p6_github_cli_submit() {
+  local msg="$1"
+
+  local token=$(p6_token_random "4")
+  local myb="branch-$token"
+
+  (
+    git checkout -b $myb
+    git add .
+    git commit -m "$1"
+    gh pr create -a $USER -f
+    git checkout master
+    git branch -D $myb
+  )
+
+  p6_return_void
+}
+
