5505807bf71e5d2198eb04be8d65f205d514013f
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-05-29 18:45:41 -0400

    bug(cli_submit): use local variable over positional arg (#8)

--- lib/cli.sh
+++ lib/cli.sh
@@ -13,11 +13,11 @@ p6_github_cli_submit() {
 
   local token=$(p6_token_random "4")
   local myb="branch-$token"
-
+  
   (
     git checkout -b $myb
     git add .
-    git commit -m "$1"
+    git commit -m "$msg"
     gh pr create -a pgollucci -f
     git checkout master
     git branch -D $myb
